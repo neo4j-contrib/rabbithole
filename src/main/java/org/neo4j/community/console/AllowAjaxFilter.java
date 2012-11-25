83d4f31dd3242331d63113f2f60c2da565d08e56
Michael Hunger <github@jexp.de>
2012-11-25 12:22:27 +0100

    added allow-origin and charset filters

---
+++ src/main/java/org/neo4j/community/console/AllowAjaxFilter.java
@@ -0,0 +1,51 @@
+package org.neo4j.community.console;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * @author mh
+ * @since 25.11.12
+ */
+public class AllowAjaxFilter implements Filter {
+    private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    private static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    private static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    private static final String ACCESS_CONTROL_REQUEST_METHOD = "access-control-request-method";
+    private static final String ACCESS_CONTROL_REQUEST_HEADERS = "access-control-request-headers";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException { }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        HttpServletResponse response= (HttpServletResponse) servletResponse;
+
+        response.addHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+
+        // Allow all forms of requests
+        final Enumeration aclMethods = request.getHeaders(ACCESS_CONTROL_REQUEST_METHOD);
+        while (aclMethods.hasMoreElements()) {
+            String value = (String) aclMethods.nextElement();
+            response.addHeader( ACCESS_CONTROL_ALLOW_METHODS, value );
+        }
+
+        // Allow all types of headers
+        final Enumeration aclHeaders = request.getHeaders(ACCESS_CONTROL_REQUEST_HEADERS);
+        while (aclHeaders.hasMoreElements()) {
+            String value = (String) aclHeaders.nextElement();
+            response.addHeader( ACCESS_CONTROL_ALLOW_HEADERS, value );
+        }
+
+        chain.doFilter(request,response);
+    }
+
+
+
+    @Override
+    public void destroy() { }
+}
---
+++ src/main/java/org/neo4j/community/console/CharsetFilter.java
@@ -0,0 +1,31 @@
+package org.neo4j.community.console;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * @author mh
+ * @since 25.11.12
+ */
+public class CharsetFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        final String contentType = servletRequest.getContentType();
+        if (contentType!=null &&     contentType.contains("text/html")) {
+            servletResponse.setContentType("text/html;charset=UTF-8");
+        }
+        if (servletResponse.getCharacterEncoding().equalsIgnoreCase("ISO-8859-1"))
+            servletResponse.setCharacterEncoding("UTF-8");
+        chain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() { }
+}
