4944d5bf768d664a1fa5abb37d696b64d8b47474
Michael Hunger <github@jexp.de>
2013-09-04 22:28:22 +0200

    some fixes to session handling

--- src/main/java/org/neo4j/community/console/SessionService.java
+++ src/main/java/org/neo4j/community/console/SessionService.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,19 +24,18 @@ class SessionService {
     private static DatabaseInfo databaseInfo;
 
     private static Map<String, Long> lastUsage = new HashMap<>();
-    private static Map<String, Neo4jService> sessions=new HashMap<>();
+    private static final Map<String, Neo4jService> sessions=new ConcurrentHashMap<>();
     private static Timer timer = new Timer(true);
     static {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 long unusedTime = System.currentTimeMillis()-TIMEOUT;
-                Iterator<Map.Entry<String, Long>> it = lastUsage.entrySet().iterator();
-                while (it.hasNext()) {
-                    Map.Entry<String, Long> entry = it.next();
+                Set<Map.Entry<String, Long>> entries = new HashSet<>(lastUsage.entrySet());
+                for (Map.Entry<String, Long> entry : entries) {
                     if (entry.getValue() < unusedTime) {
                         cleanSession(entry.getKey());
-                        it.remove();
+                        lastUsage.remove(entry.getKey());
                     }
                 }
             }
@@ -77,19 +77,16 @@ class SessionService {
         try {
             Neo4jService service = sessions.get(sessionId);
             lastUsage.put(sessionId,System.currentTimeMillis());
-            if (service != null) {
-                LOG.warn("Session found for "+sessionId+" request "+request.getRequestURI());
+            if (service != null) return service;
+            if (!create) throw new IllegalStateException("No Service for session "+sessionId+" available");
+            synchronized (sessions) {
+                service = sessions.get(sessionId);
+                if (service != null) return service;
+                LOG.warn("Session created for "+sessionId+" request "+request.getRequestURI());
+                service = createSession();
+                sessions.put(sessionId, service);
                 return service;
             }
-            if (!create) throw new IllegalStateException("No Service for session "+sessionId+" available");
-//            LOG.info("Session created for "+sessionId+" request "+request.getRequestURI());
-
-            service = databaseInfo.shouldCreateNew() ? new Neo4jService() : new Neo4jService(databaseInfo.getDatabase());
-            if (databaseInfo.shouldImport()) {
-                service.initializeFrom(SubGraph.from(databaseInfo.getDatabase()));
-            }
-            sessions.put(sessionId, service);
-            return service;
         } catch (IllegalStateException ise) {
             throw ise;
         } catch (LifecycleException | OutOfMemoryError e) {
@@ -101,23 +98,32 @@ class SessionService {
         }
     }
 
-    public static void cleanSessions() {
-        synchronized(sessions) {
-            Iterator<Map.Entry<String,Neo4jService>> it = sessions.entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry<String, Neo4jService> entry = it.next();
-                Neo4jService service = entry.getValue();
-                if (service !=null) {
-                    try {
-                        service.stop();
-                    } catch (Exception e) {
-                        // ignore
-                    }
-                }
-                it.remove();
-            }
+    private static Neo4jService createSession() throws Throwable {
+        Neo4jService service;
+        service = databaseInfo.shouldCreateNew() ? new Neo4jService() : new Neo4jService(databaseInfo.getDatabase());
+        if (databaseInfo.shouldImport()) {
+            service.initializeFrom(SubGraph.from(databaseInfo.getDatabase()));
         }
-        lastUsage.clear();
+        return service;
+    }
+
+    public static void cleanSessions() {
+        Set<Map.Entry<String, Neo4jService>> entries = new HashSet<>();
+        synchronized(sessions) {
+            entries.addAll(sessions.entrySet());
+        }
+        for (Map.Entry<String, Neo4jService> entry : entries) {
+            Neo4jService service = entry.getValue();
+            if (service !=null) {
+                try {
+                    service.stop();
+                } catch (Exception e) {
+                    // ignore
+                }
+            }
+            sessions.remove(entry.getKey());
+            lastUsage.remove(entry.getKey());
+        }
     }
 
     private static String getSessionId(HttpServletRequest request) {
