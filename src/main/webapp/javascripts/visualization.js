d4a394da804a0e00bcaa6160d41b9bc3622427aa
Michael Hunger <github@jexp.de>
2013-09-17 00:44:56 +0200

    no division by zero for overlapping nodes

--- src/main/webapp/javascripts/visualization.js
+++ src/main/webapp/javascripts/visualization.js
@@ -171,6 +171,7 @@ function GraphVisualization() {
             path_text.attr("transform", function (d) {
                 var dx = ( d.target.x - d.source.x ), dy = ( d.target.y - d.source.y );
                 var dr = Math.sqrt(dx * dx + dy * dy);
+                if (dr == 0) dr = 0.1;
                 var sinus = dy / dr;
                 var cosinus = dx / dr;
                 var l = d.type.length * 6;
