760b5355a29b8691c04f8f9dfe0375cef6efa974
Michael Hunger <github@jexp.de>
2016-04-18 08:52:53 +0200

    remove custom styling url for neod3-viz

--- src/main/webapp/javascripts/neod3-visualization.js
+++ src/main/webapp/javascripts/neod3-visualization.js
@@ -33,12 +33,12 @@ function Neod3Renderer() {
     var URLSupport = 'URL' in window && 'createObjectURL' in window.URL;
     var msBlobSupport = typeof window.navigator.msSaveOrOpenBlob !== 'undefined';
     var svgStyling = '<style>\ntext{font-family:sans-serif}\n</style>';
-    var stylingUrl = window.location.hostname === 'www.neo4j.org' ? 'http://gist.neo4j.org/css/neod3' : 'css/neod3';
-    if (window.isInternetExplorer) {
-        stylingUrl += '-ie.css';
-    } else {
-        stylingUrl += '.css';
-    }
+    //var stylingUrl = window.location.hostname === 'www.neo4j.org' ? 'http://gist.neo4j.org/css/neod3' : 'css/neod3';
+    //if (window.isInternetExplorer) {
+    //    stylingUrl += '-ie.css';
+    //} else {
+    //    stylingUrl += '.css';
+    //}
 
     var existingStyles = {};
     var currentColor = 1;
