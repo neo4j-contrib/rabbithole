876c8c091cc7e1b0628df2c538e517b65fbd4b55
Michael Hunger <github@jexp.de>
2016-05-15 23:39:46 -0500

    update to 3.0.1 apoc 1.0.0

--- src/main/java/org/neo4j/community/console/Neo4jService.java
+++ src/main/java/org/neo4j/community/console/Neo4jService.java
@@ -52,7 +52,7 @@ class Neo4jService {
             Map<String,String> config = MapUtil.stringMap("execution_guard_enabled", "true","mapped_memory_total_size","5M","dbms.pagecache.memory","5M","keep_logical_logs","false","cache_type","none","query_cache_size","15");
             GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabaseBuilder().setConfig(config).newGraphDatabase();
             Procedures procedures = ((GraphDatabaseAPI) db).getDependencyResolver().resolveDependency(Procedures.class);
-            List<Class<?>> apocProcedures = asList(Coll.class, apoc.map.MapUtil.class, Json.class, Create.class, apoc.date.Date.class, FulltextIndex.class, apoc.lock.Lock.class, LoadJson.class,
+            List<Class<?>> apocProcedures = asList(Coll.class, apoc.map.Maps.class, Json.class, Create.class, apoc.date.Date.class, FulltextIndex.class, apoc.lock.Lock.class, LoadJson.class,
                     Xml.class, PathExplorer.class, Meta.class, GraphRefactoring.class);
             apocProcedures.forEach((proc) -> {
                 try {
